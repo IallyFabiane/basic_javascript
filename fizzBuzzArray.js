0b7de89d3ccb41eeebff335606ce2ae72fb12b02
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-01-31 18:30:02 -0300

    Update fizzBuzzArray.js

--- fizzBuzzArray.js
+++ fizzBuzzArray.js
@@ -1,37 +1,21 @@
 let output = [];
 let count = 1;
 function fizzBuzz() {
-    output.push(count);
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.pop(count);
-        output.push("FizzBuzz");
-    } else if (count % 3 === 0) {
-        output.pop(count);
-        output.push("Fizz");
-    } else if (count % 5 === 0) {
-        output.pop(count);
-        output.push("Buzz");
-    } 
-    count++;
-    console.log(output);
+     while (count <= 100) {
+        output.push(count);
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.pop(count);
+            output.push("FizzBuzz");
+        } else if (count % 3 === 0) {
+            output.pop(count);
+            output.push("Fizz");
+        } else if (count % 5 === 0) {
+            output.pop(count);
+            output.push("Buzz");
+        } 
+        count++;
+        console.log(output);
+    }
 }
 
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
 fizzBuzz();
