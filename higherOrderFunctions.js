30297b070c4c6cb2e958e665e90891182e6158f8
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-02-09 18:28:17 -0300

    Add files via upload

---
+++ higherOrderFunctions.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+    
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+    
+function multiply(num1, num2) {
+        return num1 * num2;
+}
+    
+function divide(num1, num2) {
+    return num1 / num2;
+}
+    
+function calculator(num1, num2, operator) {
+    return operator(num1, num2);
+}
+
+console.log(calculator(6, 3, subtract));
+console.log(calculator(6, 3, add));
+console.log(calculator(6, 3, multiply));
+console.log(calculator(6, 3, divide));
