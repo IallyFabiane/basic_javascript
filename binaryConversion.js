1154fde502807c06db7348d3bb146f5f8aae0e3e
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-02-15 11:57:15 -0300

    Add files via upload

---
+++ binaryConversion.js
@@ -0,0 +1,7 @@
+var input = 21;
+var output1 = input.toString(2); //funciona para converter números decimais da base 2 até  a base 16!
+var output2 = input.toString(8);
+var output3 = input.toString(16);
+console.log(output1);
+console.log(output2);
+console.log(output3);
